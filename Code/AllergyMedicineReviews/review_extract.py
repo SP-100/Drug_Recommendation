6499816eeee9faa0ccd2d0d9de540405ed47aea4
Shrawan Pun <shrawan890@gmail.com>
2022-11-04 09:35:12 +0000

    Csvs scraped for backache and cold sore

--- Code/AllergyMedicineReviews/review_extract.py
+++ Code/AllergyMedicineReviews/review_extract.py
@@ -3,6 +3,7 @@ from functions import review_scrape
 # Scraping ORAL antihistamines and decongestants
 for i in range(10):
     try:
+        # antihistamine & decongestants for allergies
         review_scrape(12065, 'cetrizine', 1, 5)
         review_scrape(12127, 'zyrtec', 1, 19)
         review_scrape(148989, 'levocetirizine', 1, 4)
@@ -13,38 +14,34 @@ for i in range(10):
         review_scrape(21779, 'zyrtec-d', 1, 4)
         review_scrape(13823, 'fexofenadine', 1, 5)
         review_scrape(73, 'loratadine', 1, 6)
+        # NSAIDs for backache
+        review_scrape(5166, 'ibuprofen', 1, 14)
+        review_scrape(5173, 'naproxen', 1, 37)
+        review_scrape(1098, 'aleve', 1, 10)
+        review_scrape(362, 'acetaminophen', 1, 7)
+        review_scrape(7076, 'tylenol', 1, 4)
+        # Antiviral Medications for cold sore
+        review_scrape(941, 'acyclovir', 1, 19)
+        review_scrape(6279, 'valacyclovir', 1, 9)
+        review_scrape(6157, 'famciclovir', 1, 4)
     finally:
         break
 
 
+# Merging all csv files into one
 import pandas as pd
 import os
+os.getcwd()
+
+# Change cwd to the reviews folder
+# path = r"C:\Users\ShrawanPun\Drug Recommendation Project\Code\AllergyMedicineReviews"
+# os.chdir(path)
 
 master_df = pd.DataFrame()
 
 for file in os.listdir(os.getcwd()):
     if file.endswith('.csv'):
-        master_df = master_df.concat(pd.read_csv(file))
+        master_df = pd.concat([pd.read_csv(file)])
 
-master_df.to_csv('Allergy medicine reviews.csv', index=False)
+master_df.to_csv('MedicineReviews.csv', index=False)
 
-# # ANTIHISTAMINES
-
-# # oral
-# cetrizine = "https://www.webmd.com/drugs/drugreview-12065-cetirizine-oral?conditionid=&sortval=1&page="
-# zyrtec = "https://www.webmd.com/drugs/drugreview-12127-zyrtec-oral?conditionid=&sortval=1&page="
-# levocetirizine = "https://www.webmd.com/drugs/drugreview-148989-levocetirizine-oral?conditionid=&sortval=1&page="
-# xyzal = "https://www.webmd.com/drugs/drugreview-148996-xyzal-oral?conditionid=&sortval=1&page="
-# fexofenadine = "https://www.webmd.com/drugs/drugreview-13823-fexofenadine-oral?conditionid=&sortval=1&page="
-# diphenhydramine = "https://www.webmd.com/drugs/drugreview-1428-diphenhydramine-oral?conditionid=&sortval=1&page="
-# benadryl = "https://www.webmd.com/drugs/drugreview-5680-benadryl-oral?conditionid=&sortval=1&page="
-# clarinex = "https://www.webmd.com/drugs/drugreview-22326-clarinex-oral?conditionid=&sortval=1&page="
-
-
-# # DECONGESTANTS
-
-# # oral
-# zyrtec-D = "https://www.webmd.com/drugs/drugreview-21779-zyrtec-d-oral?conditionid=&sortval=1&page="
-# fexofenadine = "https://www.webmd.com/drugs/drugreview-13823-fexofenadine-oral?conditionid=&sortval=1&page="
-# loratadine = "https://www.webmd.com/drugs/drugreview-73-loratadine-oral?conditionid=&sortval=1&page="
-# semprex-d = "https://www.webmd.com/drugs/drugreview-410-semprex-d-oral?conditionid=&sortval=1&page="
